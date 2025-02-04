f01839cfa6d838885c09526aa6d8612e6404efbc
Jaowei <jaowei.8@gmail.com>
2025-02-04 22:36:20 +0800

    add:handle multiple cards in dbs statement

--- src/lib/parsers/pdf/formats/dbs.ts
+++ src/lib/parsers/pdf/formats/dbs.ts
@@ -12,10 +12,17 @@ export const isDBSCardFormat: PDFFormatChecker = (data) => {
 };
 
 const filterTextData = (text: string): boolean => {
-  if (!text || text === " " || text.length > 40 || text.includes("NEW TRANSACTIONS")) {
+  if (!text || text === " " || text.length > 80 || text.includes("NEW TRANSACTIONS")) {
     return true;
   }
   return false;
+};
+
+const isCardName = (text: string): string | undefined => {
+  if (text.includes("CARD NO.: ")) {
+    return text.substring(0, text.length - 25);
+  }
+  return;
 };
 
 const getYear = (text: string): string => {
@@ -32,6 +39,8 @@ const extractAmount = (row: Array<string> | string) => {
   if (lastItem === "CR") {
     const secondLastItem = row.at(-2)?.replace(",", "");
     parsedAmount = parseFloat(secondLastItem ?? "0.0");
+  } else if (Number.isNaN(parsedAmount)) {
+    return undefined;
   }
   return parsedAmount;
 };
@@ -41,9 +50,15 @@ const extractDate = (row: Array<string> | string, year: string) => {
   return formatTransactionDate(rawString, "DD MMMYYYY");
 };
 
-const parseAppRow = (data: { row: string[]; year: string; accountName: string; companyName: string }): Transaction => {
+const parseAppRow = (data: {
+  row: string[];
+  year: string;
+  accountName: string;
+  companyName: string;
+}): Transaction | undefined => {
   const { row, year, accountName, companyName } = data;
   const parsedAmount = extractAmount(row);
+  if (!parsedAmount) return undefined;
 
   const date = extractDate(row, year);
 
@@ -71,6 +86,8 @@ export const parseDBSFormat: PDFFormatParser = (textData, accountName, companyNa
   let prevIdx: number = 0;
   let row: Array<string> = [];
   let result: Array<Transaction> = [];
+  // TODO: Will always be overriden for now, determine proper fallback approach here
+  let baseAccountName = accountName;
 
   for (let i = 0; i < textData.length; i++) {
     const prevData = textData[prevIdx];
@@ -82,6 +99,11 @@ export const parseDBSFormat: PDFFormatParser = (textData, accountName, companyNa
 
     if (!statementYear) statementYear = getYear(text);
 
+    const cardName = isCardName(text);
+    if (cardName) {
+      baseAccountName = cardName;
+    }
+
     if (text === "PREVIOUS BALANCE") {
       headerCoord = data.transform[5];
     }
@@ -90,7 +112,7 @@ export const parseDBSFormat: PDFFormatParser = (textData, accountName, companyNa
     if (headerCoord === 0) continue;
 
     // Stop processing
-    if (text === "TOTAL:") break;
+    if (text.includes("GRAND TOTAL FOR ALL CARD ACCOUNTS:")) break;
 
     const prevCoord = prevData.transform[5];
     const currentCoord = data.transform[5];
@@ -103,10 +125,10 @@ export const parseDBSFormat: PDFFormatParser = (textData, accountName, companyNa
         const parsedData = parseAppRow({
           row,
           year: statementYear,
-          accountName,
+          accountName: baseAccountName,
           companyName,
         });
-        result.push(parsedData);
+        if (parsedData) result.push(parsedData);
       }
       row = [text];
     }
